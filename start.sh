ef267db10f79cda0c322a33a82a0b3bdaa13c3b7
abhiii1801 <140613897+abhiii1801@users.noreply.github.com>
2025-07-21 00:27:45 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Installing Playwright..."
-playwright install
 
-# Then start the app
+echo "Installing Playwright browsers..."
+npx playwright install --with-deps
+
+echo "Starting FastAPI server..."
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
